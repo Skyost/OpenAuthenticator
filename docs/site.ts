59293de72c8de7bed084e38623d30b0382a4832c
Hugo Delaunay <Skyost@users.noreply.github.com>
2024-07-09 17:49:45 +0200

    feat(docs): Added a link to the App Store.

--- docs/site.ts
+++ docs/site.ts
@@ -1,5 +1,5 @@
 export interface StoreInfo {
-  name: string,
+  name: string
   url: string | null
 }
 
@@ -8,27 +8,27 @@ export type OS = 'android' | 'darwin' | 'windows' | 'linux'
 export const storesLink: Record<OS, StoreInfo> = {
   android: {
     name: 'Google Play',
-    url: null // 'https://play.google.com/store/apps/details?id=app.openauthenticator'
+    url: null, // 'https://play.google.com/store/apps/details?id=app.openauthenticator',
   },
   darwin: {
     name: 'App Store',
-    url: null // 'https://apps.apple.com/app/id6479272927'
+    url: 'https://apps.apple.com/app/id6479272927',
   },
   windows: {
     name: 'Microsoft Store',
-    url: null // 'https://www.microsoft.com/store/apps/9PB8HFZFKLT4'
+    url: null, // 'https://www.microsoft.com/store/apps/9PB8HFZFKLT4',
   },
   linux: {
     name: 'Snapcraft',
-    url: null
-  }
+    url: null,
+  },
 }
 
 export const siteMeta = {
   name: 'Open Authenticator',
   description: 'Secure your online accounts with a free, open-source and lovely-crafted app.',
   url: 'https://openauthenticator.app',
-  github: 'https://github.com/Skyost/OpenAuthenticator'
+  github: 'https://github.com/Skyost/OpenAuthenticator',
 }
 
 export const contactPostUrl = 'https://script.google.com/macros/s/AKfycbzXs_vsLAX5jStwqH9mIVKr8mr7L0C3wsVC4net9BlvhbYFR97UkE9tRIsT2z07REKjQQ/exec'
