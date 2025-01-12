78c5cf3f04b64e1e8e086df4f34cea4002d87908
Hugo Delaunay <Skyost@users.noreply.github.com>
2025-01-12 21:55:18 +0100

    fix: Better handling of HTTPS links in the app settings.

--- lib/pages/settings/entries/github.dart
+++ lib/pages/settings/entries/github.dart
@@ -1,31 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:open_authenticator/app.dart';
 import 'package:open_authenticator/i18n/translations.g.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:open_authenticator/pages/settings/entries/uri_settings_entry.dart';
 
 /// Takes the user to Github to report bugs, suggest new features, ...
-class GithubSettingsEntryWidget extends StatelessWidget {
+class GithubSettingsEntryWidget extends UriSettingsEntry {
   /// Creates a new Github settings entry widget instance.
-  const GithubSettingsEntryWidget({
+  GithubSettingsEntryWidget({
     super.key,
-  });
+  }) : super(
+          icon: Icons.bug_report,
+          title: translations.settings.about.github.title,
+          subtitle: translations.settings.about.github.subtitle,
+          uri: Uri.parse(App.githubRepositoryUrl),
+        );
 
   @override
-  Widget build(BuildContext context) => ListTile(
-    leading: const Icon(Icons.bug_report),
-    title: Text(translations.settings.about.github.title),
-    subtitle: Text(translations.settings.about.github.subtitle),
-    onTap: () async {
-      Uri uri = Uri.parse(App.githubRepositoryUrl);
-      Uri withFragment = Uri(
-        scheme: uri.scheme,
-        host: uri.host,
-        path: uri.path,
-        fragment: 'report-bugs-or-suggest-new-features',
-      );
-      if (await canLaunchUrl(withFragment)) {
-        launchUrl(withFragment);
-      }
-    },
-  );
+  Uri get uri {
+    Uri uri = super.uri;
+    return Uri(
+      scheme: uri.scheme,
+      host: uri.host,
+      path: uri.path,
+      fragment: 'report-bugs-or-suggest-new-features',
+    );
+  }
 }
--- lib/pages/settings/entries/translate.dart
+++ lib/pages/settings/entries/translate.dart
@@ -1,25 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:open_authenticator/app.dart';
 import 'package:open_authenticator/i18n/translations.g.dart';
+import 'package:open_authenticator/pages/settings/entries/uri_settings_entry.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 /// Takes the user to the app translation page.
-class TranslateSettingsEntryWidget extends StatelessWidget {
+class TranslateSettingsEntryWidget extends UriSettingsEntry {
   /// Creates a new translate settings entry widget instance.
-  const TranslateSettingsEntryWidget({
+  TranslateSettingsEntryWidget({
     super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) => ListTile(
-    leading: const Icon(Icons.translate),
-    title: Text(translations.settings.about.translate.title),
-    subtitle: Text(translations.settings.about.translate.subtitle(appName: App.appName)),
-    onTap: () async {
-      Uri uri = Uri.parse(App.appTranslationUrl);
-      if (await canLaunchUrl(uri)) {
-        launchUrl(uri);
-      }
-    },
-  );
+  }) : super(
+          icon: Icons.translate,
+          title: translations.settings.about.translate.title,
+          subtitle: translations.settings.about.translate.subtitle(appName: App.appName),
+          uri: Uri.parse(App.appTranslationUrl),
+        );
 }
